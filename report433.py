0433f04fef27422420a61ef8817833e99af7f42a
root <root@raspberrypi.(none)>
2015-11-04 23:19:14 +0100

    report433.py updated to include nma

--- report433.py
+++ report433.py
@@ -5,19 +5,17 @@ import os
 import ConfigParser
 from subprocess import Popen, PIPE
 import urllib2
-
+from pynma import PyNMA
+import shelve
 
 def reportSensor( sensorId ):
 
-        config = ConfigParser.ConfigParser()
-        config.read("/home/pi/Surveillance/uploader-no-ip.cfg")
+        myconfig = ConfigParser.ConfigParser()
+        myconfig.read("/home/pi/Surveillance/uploader-no-ip.cfg")
 
         #Domoticz account credentials
-        username = config.get('domoticz', 'user')
-        password = config.get('domoticz', 'password')
-
-	print username
-	print password
+        username = myconfig.get('domoticz', 'user')
+        password = myconfig.get('domoticz', 'password')
 
 	output = ''
 	p = urllib2.HTTPPasswordMgrWithDefaultRealm()
@@ -30,16 +28,25 @@ def reportSensor( sensorId ):
 	if len(output)>0:
 		csv_data = output.split(",")
                 humStat = 0
+
 	        if (sensorId==2):
-			if (csv_data[3] < 40):	humStat = 2
-			elif(csv_data[3] > 70):	humStat = 3
-	                url = "http://caquiroga.no-ip.biz:8081/json.htm?type=command&param=udevice&idx=13&nvalue=0&svalue=" +  str(csv_data[2]) + ";" + str(csv_data[3]) + ";" + str(humStat) + ";"
+			if (float(csv_data[3]) < 40):	humStat = 2
+			elif(float(csv_data[3]) > 70):	humStat = 3
+	                url = "http://caquiroga.no-ip.biz:8081/json.htm?type=command&param=udevice&idx=8&nvalue=0&svalue=" +  str(csv_data[2]) + ";" + str(csv_data[3]) + ";" + str(humStat) + ";"
 	        elif sensorId==1:
-                        if (csv_data[3] < 40):      humStat = 2
-                        elif(csv_data[3] > 70):     humStat = 3
-	                url = "http://caquiroga.no-ip.biz:8081/json.htm?type=command&param=udevice&idx=12&nvalue=0&svalue=" +  str(csv_data[2]) + ";" + str(csv_data[3]) + ";" + str(humStat) + ";"
+                        if (float(csv_data[3]) < 40):      humStat = 2
+                        elif(float(csv_data[3]) > 70):     humStat = 3
+	                url = "http://caquiroga.no-ip.biz:8081/json.htm?type=command&param=udevice&idx=9&nvalue=0&svalue=" +  str(csv_data[2]) + ";" + str(csv_data[3]) + ";" + str(humStat) + ";"
 	        elif sensorId==10:
-        	        url = "http://caquiroga.no-ip.biz:8081/json.htm?type=command&param=udevice&idx=15&nvalue=0&svalue=0;" +  str(csv_data[2]) + ";"
+        	        nma = PyNMA()
+			temp = shelve.open("/home/pi/Surveillance/tmp/raintemp")
+			nma.addkey(temp['nmakey'])
+			print temp['nmakey']
+        	        url = "http://caquiroga.no-ip.biz:8081/json.htm?type=command&param=udevice&idx=11&nvalue=0&svalue=0;" + str(csv_data[2]) + ";"
+			if (float(csv_data[2]) > temp['rain']+2): nma.push("Maceda", 'Plou molt a Maceda', '', '', batch_mode=False)
+			elif (float(csv_data[2]) > temp['rain']): nma.push("Maceda", 'Plou a Maceda', '', '', batch_mode=False)
+			temp['rain'] = float(csv_data[2])		
+			temp.close()
 	        print url
 	        p.add_password(None, url, username, password)
 	        handler = urllib2.HTTPBasicAuthHandler(p)
@@ -53,6 +60,5 @@ try:
 	reportSensor(1);
 	reportSensor(2);
 	reportSensor(10);
-
 except Exception,e: print str(e)
 
